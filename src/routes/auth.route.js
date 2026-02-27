831ba74f75c5adc3507d8789afda631a3df5f9b6
Ishrz <ishrzz66@gmail.com>
2026-02-27 23:48:12 +0530

    added getMe api

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const {registraion, login} = require("../controllers/auth.controller.js")
+
 const logger = require("../middlewares/logger.middleware.js")
+const tokenVerification = require("../middlewares/auth.middleware.js")
 
 const authRoute= express.Router()
 
@@ -9,6 +11,8 @@ const authRoute= express.Router()
 authRoute.post("/registration", logger , registraion)
 
 authRoute.post("/login", logger , login)
+
+authRoute.get("/get-me", logger , tokenVerification,  getMe)
 
 
 module.exports=authRoute
